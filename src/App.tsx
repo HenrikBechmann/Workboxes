0cfff3d2e66fcea38eb16ff46c9e6d5a667726d4
Henrik Bechmann <henrik@therefore.ca>
2023-11-13 00:25:12 -0500

    introduce main Tribalopolis component

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,16 @@ import React, { useState, useEffect } from 'react'
 import { getAuth } from 'firebase/auth'
 import { initializeFirestore, enableIndexedDbPersistence } from 'firebase/firestore'
 // import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check'
-// import FirestoreSettings from 'firebase-admin'
+// import FirestoreSettings from 'firebase-admin' // fail
+
+// reactfire
+import { useFirebaseApp, AuthProvider, useInitFirestore, FirestoreProvider, AppCheckProvider } from 'reactfire'
+
+// local
+import Tribalopolis from './Tribalopolis'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-// for AppCheck debug: 005B512D-5DE0-4D2C-A2E5-0878A53A514A??
-// reactfire
-import { useFirebaseApp, AuthProvider, useInitFirestore, FirestoreProvider, AppCheckProvider } from 'reactfire'
 
 // chakra
 import { ChakraProvider } from '@chakra-ui/react'
@@ -23,13 +26,13 @@ const App = () => {
 
     const app = useFirebaseApp()
 
-    // TODO failed appcheck attempts
+    // ------------- TODO failed appcheck attempts --------------
     // self['FIREBASE_APPCHECK_DEBUG_TOKEN'] = true;
+    // for AppCheck debug: 005B512D-5DE0-4D2C-A2E5-0878A53A514A??
     // const APP_CHECK_TOKEN = '005B512D-5DE0-4D2C-A2E5-0878A53A514A' // for debug??
     // const APP_CHECK_TOKEN = 'B2A3D5A5-0F86-49D5-88BD-F7D66622468F' // debug
     // error: App Check debug token: bf1e4747-fb4a-4d04-8a0c-2b53cb1b63b7. // not entered
     // You will need to add it to your app's App Check settings in the Firebase console for it to work.
-
     // const APP_CHECK_TOKEN = "6LdJ9gwpAAAAAAbTlVr_PRdMLcwhFSfbSO_AruxN" // for tribalopolis-dev.firebaseapp.com
     // const appCheck = initializeAppCheck(app, {
     //     provider: new ReCaptchaV3Provider(APP_CHECK_TOKEN), // generates 400 error
@@ -42,13 +45,13 @@ const App = () => {
 
     const { status:firestoreStatus, data: firestoreInstance } = useInitFirestore(async (firebaseApp) => {
         const db = initializeFirestore(firebaseApp, {})
-        await enableIndexedDbPersistence(db) // TODO console message says to be deprecated 
+        await enableIndexedDbPersistence(db) // TODO console message says this is to be deprecated...
             // use `FirestoreSettings.cache` instead -- firebase-admin? source and specs not apparent
-        // FirestoreSettings.firestore // failed attempt
+            // FirestoreSettings.firestore // failed attempt
         return db
     })
 
-    useEffect(()=>{
+    useEffect(()=>{ // TODO revise for all possibilities; add SDKs
 
         if (firestoreStatus !== 'success') { // 'success', 'loading', 'error'
             setAppState(firestoreStatus)
@@ -65,7 +68,7 @@ const App = () => {
             {appState == 'ready'
                 ? <FirestoreProvider sdk={firestoreInstance}>
                     <ChakraProvider>
-                        <div>Hello, global world!</div>
+                        <Tribalopolis />
                     </ChakraProvider>
                   </FirestoreProvider>
                 : <div>Waiting...</div>
