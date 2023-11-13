e756da6a802e8d36a7fd8ab220ac5eadb0e4380e
Henrik Bechmann <henrik@therefore.ca>
2023-11-13 00:26:26 -0500

    introduce main Tribalopolis component

---
+++ src/Tribalopolis.tsx
@@ -0,0 +1,13 @@
+// Tribalopolis.tsx
+// copyright (c) 2023-present Henrik Bechmann, Toronto, Licence: GPL-3.0
+
+import React, { useState, useEffect } from 'react'
+
+export const Tribalopolis = (props) => {
+
+
+    return <div>Hello, global world!</div>
+
+}
+
+export default Tribalopolis
