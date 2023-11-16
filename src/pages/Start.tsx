3c312b38a2f09c343ade597816dd73ef6e178c24
Henrik Bechmann <henrik@therefore.ca>
2023-11-15 20:17:48 -0500

    start page

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -5,7 +5,11 @@ import React, { useState, useEffect } from 'react'
 
 export const Start = (props) => {
 
-    return <div>Use Tribalopolis to Organize, Re-organize, Communicate, Collborate</div>
+    return <>
+        <p>Use Tribalopolis to Organize, Re-organize, Communicate, Collborate</p>
+        <p>While we get established, we're accepting members by invitation only.</p>
+        <p>If you've received an invitation, please sign up or log in.</p>
+    </>
 
 }
 
