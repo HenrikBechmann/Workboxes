36be6c91efd6e0d8c717430ed51c46eb4f778c75
Henrik Bechmann <henrik@therefore.ca>
2023-11-16 07:34:04 -0500

    baseline

---
+++ src/Start.tsx
@@ -0,0 +1,37 @@
+// Start.tsx
+// copyright (c) 2023-present Henrik Bechmann, Toronto, Licence: GPL-3.0
+
+import React from 'react'
+import { 
+    Card, CardHeader, CardBody, CardFooter,
+    Box, Stack, StackDivider,
+    Text, Heading, Image
+} from '@chakra-ui/react'
+
+export const Start = (props) => {
+
+    return ( 
+    <Card margin = {3}>
+    
+        <CardBody>
+            <Stack divider={<StackDivider />} spacing='4'>
+            <Box>
+                <Heading size = 'xl'>
+                    <Image src = '/public/icons/fire.png'/> Use Tribalopolis to...
+                </Heading>
+            </Box>
+            <Box>
+                <Heading size = 'lg'> Organize, Re-organize, Communicate, Collaborate</Heading>
+            </Box>
+            <Box>
+                <Text>While we get established, we're accepting members by invitation only.</Text>
+                <Text>If you've received an invitation, please sign up or log in.</Text>
+            </Box>
+            </Stack>
+        </CardBody>
+    
+    </Card>
+    )
+}
+
+export default Start
