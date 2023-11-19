481652a46aa86fb567df7a4e8a47c38d268147cb
Henrik Bechmann <henrik@therefore.ca>
2023-11-19 02:55:19 -0500

    add declined page

---
+++ app/src/pages/Declined.tsx
@@ -0,0 +1,12 @@
+// Declined.tsx
+// copyright (c) 2023-present Henrik Bechmann, Toronto, Licence: GPL-3.0
+
+import React from 'react'
+
+const Declined = (props) => {
+
+    return <div>Declined</div>
+
+}
+
+export default Declined
