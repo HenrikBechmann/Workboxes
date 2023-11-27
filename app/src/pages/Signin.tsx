a323efff59db2608388bd4462ed91fe4821e672e
Henrik Bechmann <henrik@therefore.ca>
2023-11-27 01:41:43 -0500

    signin text

--- app/src/pages/Signin.tsx
+++ app/src/pages/Signin.tsx
@@ -116,17 +116,15 @@ const Signin = (props) => {
             </Box>
             <Box style = {{textAlign:'center'}} >
                 <Text>Self organize into Work Groups.</Text>
-                <Text>Organize information into 
-                    <Image style = {{display:'inline-block', verticalAlign:'middle'}} src = {boxIcon}/> 
-                    Work Boxes.
+                <Text>Organize information into Work Boxes. &nbsp;
+                    <Image style = {{display:'inline-block', verticalAlign:'middle'}} src = {boxIcon}/>
                 </Text>
-                <Text>Cluster Work Boxes into Work Windows.</Text>
+                <Text>Organize Work Boxes into Work Windows,</Text>
+                <Text>Work Panels, and Work Spaces.</Text>
                 <Text>Drag and Drop 
                     <Image style = {{display:'inline-block', verticalAlign:'middle'}} src = {dragIcon}/> 
                     things around to keep up to date.
                 </Text>
-                <Text>Organize Work Windows into Work Panels.</Text>
-                <Text>Organize Work Panels into Work Spaces.</Text>
                 <Text>Have fun!</Text>
             </Box>
             </VStack>
