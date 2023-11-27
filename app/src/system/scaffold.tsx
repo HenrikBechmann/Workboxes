12a10d665f5fb36adb197b4f26009d8ac32b51ed
Henrik Bechmann <henrik@therefore.ca>
2023-11-27 03:58:33 -0500

    change router list name to scaffold

---
+++ app/src/system/scaffold.tsx
@@ -0,0 +1,39 @@
+// Routes.tsx
+// copyright (c) 2023-present Henrik Bechmann, Toronto, Licence: GPL-3.0
+
+import React from 'react'
+
+import MemberRoutes from '../components/MemberRoutes'
+import Tribalopolis from '../Tribalopolis'
+import Signin from '../pages/Signin'
+import Account from '../pages/Account'
+import NotFound from '../pages/NotFound'
+
+const routes = [
+    
+    {
+        path: '/',
+        element: <MemberRoutes />,
+        children: [
+            {
+                index: true,
+                element: <Tribalopolis />,
+            },
+            {
+                path: 'account',
+                element: <Account />,
+            },
+            {
+                path: '*',
+                element: <NotFound />,
+            },
+        ],
+    },
+    {
+        path: 'signin',
+        element: <Signin />
+    }
+
+]
+
+export default routes
