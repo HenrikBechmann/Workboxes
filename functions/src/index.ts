36f1bb06f818a08040b88257c251bcd52bcc826f
Henrik Bechmann <henrik@therefore.ca>
2023-11-28 05:29:17 -0500

    function attempt

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,7 +7,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-// import {onRequest} from "firebase-functions/v2/https";
+import {HttpsError} from "firebase-functions/v2/https";
 // import * as logger from "firebase-functions/logger";
 
 import {getFirestore} from "firebase-admin/firestore";
@@ -21,20 +21,28 @@ import {
 // https://firebase.google.com/docs/functions/typescript
 
 export const beforecreated = beforeUserCreated((event) => {
+  const user = event.data;
+  let email = user?.email;
+  if (email) email = email.toLowerCase();
   const db = getFirestore();
+  console.log("retrieving record");
   db.collection("invitations")
-    .where("email", "==", "something".toLowerCase())
+    .where("email", "==", email)
     .get()
     .then((snapshot) => {
+      console.log("finished query");
       if (snapshot.empty) {
-        // fail!
+        throw new HttpsError("permission-denied",
+          "An invitation is required to sign in to Tribalopolis",
+          email + " is not found in invitations");
       }
     })
     .catch((error) => {
-      // fail with error
+      console.log("query error");
+      throw new HttpsError("internal",
+        "Internal error: " + error.message,
+        email + " is not found in invitations");
     });
-
-  return;
 });
 
 export const beforesignedin = beforeUserSignedIn((event) => {
