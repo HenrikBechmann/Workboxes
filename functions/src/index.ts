aec9d1b79a453693563e0798a17ce72ea0f6c8d5
Henrik Bechmann <henrik@therefore.ca>
2023-11-28 21:30:31 -0500

    code maintenance

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,9 +8,6 @@
  */
 
 import {HttpsError} from "firebase-functions/v2/https";
-// import {config} from "firebase-functions";
-// import * as logger from "firebase-functions/logger";
-// import {initializeApp} from "firebase-admin";
 import * as admin from "firebase-admin";
 const app = admin.initializeApp();
 import {getFirestore} from "firebase-admin/firestore";
@@ -25,24 +22,16 @@ export const beforecreated = beforeUserCreated(async (event) => {
   const user = event.data;
   let email = user?.email;
   if (email) email = email.toLowerCase();
-  // console.log("email", email);
   const db = getFirestore(app);
-  // console.log("retrieving record");
   let result;
   try {
     result = await db.collection("invitations")
       .where("email", "==", email).get();
   } catch (error) {
-    // console.log("query error", error);
     // throw new HttpsError("internal",
     //   "Internal error: " + error.message,
     //   email + " is not found in invitations");
   }
-  // .then((snapshot) => {
-  //   console.log("finished query: snapshot.docs[0]", snapshot.docs[0]);
-  //   return snapshot.docs[0];
-  // })
-  // console.log("query result", result?.docs[0]);
   if (!result?.docs[0]) {
     throw new HttpsError("permission-denied",
       "An invitation is required to sign in to Tribalopolis.",
