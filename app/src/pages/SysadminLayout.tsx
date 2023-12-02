35e11418af6eba2cb8045f21ffa92965b0c7cc70
Henrik Bechmann <henrik@therefore.ca>
2023-12-02 09:59:00 -0500

    code maintenance

--- app/src/pages/SysadminLayout.tsx
+++ app/src/pages/SysadminLayout.tsx
@@ -6,16 +6,19 @@ import { Outlet } from 'react-router-dom'
 
 import Toolbar from '../components/Toolbar'
 
-const bodyStyle = {height: 'calc(100vh - 56px)', display:'relative', backgroundColor:'gray'}
+const bodyStyle = {
+    height: 'calc(100vh - 28px)', 
+    display:'relative', 
+    backgroundColor:'gray'
+}
 
 const SysadminLayout = (props) => {
 
     return <>
-        <div data-type = 'sysadmin-layout' style = {{height:'28px'}}><Toolbar /></div>
+        <Toolbar />
         <div data-type = 'sysadmin-outlet' style = {bodyStyle}>
             <Outlet />
         </div>
-        <div style = {{height:'28px'}}>Footer</div>
     </>
 
 }
