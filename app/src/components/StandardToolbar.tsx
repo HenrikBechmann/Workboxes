41e862fbc4ab15c4a542704a02638bc1935023ba
Henrik Bechmann <henrik@therefore.ca>
2023-12-02 23:21:56 -0500

    isolate menu portion of the standard toolbar

--- app/src/components/StandardToolbar.tsx
+++ app/src/components/StandardToolbar.tsx
@@ -26,6 +26,43 @@ const standardToolbarStyles = {
 
 } as CSSProperties
 
+const FireIconControl = (props) => {
+    
+    return <div style = {
+        {
+            display:'flex',
+            // backgroundColor:'lightgray',
+            padding:'2px',
+            borderRadius:'6px',
+            marginLeft:'6px',
+        }
+    } ><img src = {fireIcon} /><span style = {{opacity:0.5}} >▼</span></div> 
+
+}
+
+const UserControl = (props) => {
+
+    const 
+        userData = useUserData(),
+        { displayName, photoURL } = userData.authUser
+
+    return <div style = {
+        {
+            display:'flex',
+            alignItems:'center',
+            marginLeft:'12px',
+            borderRadius:'6px',
+            // opacity:0.7,
+            // border:'1px solid gray',
+        }
+    }>
+        <img style = {{width:'24px', height:'24px', borderRadius:'12px'}} src = {photoURL} />
+        <div style = {{fontSize:'small', marginLeft:'4px',marginRight:'3px', whiteSpace:'nowrap'}} >{displayName}</div>
+        <span style = {{opacity:0.5}} >▼</span>
+    </div>
+
+}
+
 const StandardToolbar = (props) => {
 
     const 
@@ -43,15 +80,7 @@ const StandardToolbar = (props) => {
     }
 
     return <div style = {standardToolbarStyles}>
-        <div style = {
-            {
-                display:'flex',
-                // backgroundColor:'lightgray',
-                padding:'2px',
-                borderRadius:'6px',
-                marginLeft:'6px',
-            }
-        } ><img src = {fireIcon} /><span style = {{opacity:0.5}} >▼</span></div> 
+        <FireIconControl />
         <div style = {{marginLeft:'12px',opacity:0.7}} >
             <img src = {notificationsIcon} />
         </div> 
@@ -68,21 +97,8 @@ const StandardToolbar = (props) => {
         <div style = {{marginLeft:'12px',opacity:0.7}} onClick = {goHome}>
             <img src = {homeIcon} />
         </div>
+        <UserControl />
         <div style = {{height:'20px',borderLeft:'1px solid gray', width:'0px', marginLeft:'12px'}}></div>
-        <div style = {
-            {
-                display:'flex',
-                alignItems:'center',
-                marginLeft:'12px',
-                borderRadius:'6px',
-                // opacity:0.7,
-                // border:'1px solid gray',
-            }
-        }>
-            <img style = {{width:'24px', height:'24px', borderRadius:'12px'}} src = {photoURL} />
-            <div style = {{fontSize:'small', marginLeft:'4px',marginRight:'3px', whiteSpace:'nowrap'}} >{displayName}</div>
-            <span style = {{opacity:0.5}} >▼</span>
-        </div>
         {isSuperUser && <>
             <div style = {{height:'20px',borderLeft:'1px solid gray', width:'0px', marginLeft:'12px'}}></div>
             <div style = {{marginLeft:'12px',opacity:0.7}} onClick = {gotoSysadmin}>
