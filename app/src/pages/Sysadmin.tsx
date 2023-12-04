117955ab7f1348f2391ff5a7e48f7c41b917445f
Henrik Bechmann <henrik@therefore.ca>
2023-12-03 22:57:54 -0500

    code maintenance

--- app/src/pages/Sysadmin.tsx
+++ app/src/pages/Sysadmin.tsx
@@ -12,29 +12,33 @@ import {
     Center, Flex
 } from '@chakra-ui/react'
 
+const cardStyles = {
+    cursor:'pointer',
+}
 
 const MenuTile = (props) => {
     const {header, body, buttonPrompt, nav} = props
 
     const navigate = useNavigate()
 
-    const gotoNav = () => {
+    const gotoNav = (e) => {
+        e.preventDefault()
         navigate(nav)
     }
 
-    return <Card boxShadow = {'0px 0px 15px gray'} h = '100%'>
+    return <Card onClick = {gotoNav} style = {cardStyles} boxShadow = {'0px 0px 15px gray'} h = '100%'>
         <CardHeader p = '3px'>
             <Heading fontSize = 'xl' textAlign = 'center'>{header}</Heading>
         </CardHeader>
         <CardBody p = '3px 12px'>
             <Text>{body}</Text>
         </CardBody>
-        <CardFooter justifyContent = 'center'>
+{/*        <CardFooter justifyContent = 'center'>
             <Button onClick = {gotoNav}>
                 {buttonPrompt}
             </Button>
         </CardFooter>
-    </Card>
+*/}    </Card>
 }
 
 const menuTileData = [
@@ -42,25 +46,25 @@ const menuTileData = [
             header:'Global settings',
             body:`One document holds global settings for clients; 
                 another holds internal global settings.`,
-            buttonPrompt:'Go to global settings',
+            buttonPrompt:'Administer',
             nav:'/sysadmin/settings',
         },
         {
             header:'Document metadata',
             body:`Technical parameters for each data type, informing clients how to present fields.`,
-            buttonPrompt:'Go to metadata',
+            buttonPrompt:'Administer',
             nav:'/sysadmin/metadata',
         },
         {
             header:'User controls',
             body:`Invitations and suspensions, by email.`,
-            buttonPrompt:'Go to User controls',
+            buttonPrompt:'Administer',
             nav:'/sysadmin/usercontrols',
         },
         {
             header:'Help panels',
             body:`Sets the text for each help icon. Help icons are found all over the place.`,
-            buttonPrompt:'Go to help panels',
+            buttonPrompt:'Administer',
             nav:'/sysadmin/helppanels',
         },
     ]
@@ -90,7 +94,7 @@ const Sysadmin = (props) => {
           <GridItem area={'header'}>
               <Center>
                   <VStack>
-                  <Heading mt = {1} fontSize = 'xl'>System Administration</Heading>
+                  <Heading mt = {1} fontSize = 'xl'>System Administration Menu</Heading>
                   <Heading mt = {0} fontSize = 'md' color = 'gray'>Available only to system administrators</Heading>
                   </VStack>
               </Center>
