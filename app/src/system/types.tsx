8578c51d2928584723230b552cc51549365386d0
Henrik Bechmann <henrik@therefore.ca>
2023-12-04 21:15:55 -0500

    update types concept

--- app/src/system/types.tsx
+++ app/src/system/types.tsx
@@ -3,51 +3,78 @@
 // example
 // lookup is a namespace, parse with lowdash
 const Person = {
-  definitions: {
-    Person: {
-      properties: {
-        age: {
-          type: "number",
-          default:{
-              value:null,
-              lookup:null
-          }
-        },
-        firstName: {
-          type: "string",
-          default:{
-              value:null,
-              lookup:null
-          }
-        },
-        lastName: {
-          type: "string",
-          default:{
-              value:null,
-              lookup:null
-          }
-        },
-        socials: {
-          items: {
-            type: "string",
-              default:{
-                  value:null,
-                  lookup:null
-              }
-          },
-          type: "array",
-          default:{
-              value:null,
-              lookup:null
-          },
-        }
-      },
-      required: [ "firstName", "lastName", "age", "socials"],
-      type: "object",
+  properties: {
+    age: {
+      type: "number",
       default:{
           value:null,
           lookup:null
       },
+      alias:'Age',
+      order:1,
+      prompt:'',
+      description:'',
+      errormessage:'',
+      isValid:{},
+      onChange:{}
+    },
+    firstName: {
+      type: "string",
+      default:{
+          value:null,
+          lookup:null
+      },
+      alias:'Age',
+      order:1,
+      prompt:'',
+      description:'',
+      errormessage:'',
+      isValid:{},
+      onChange:{}
+    },
+    lastName: {
+      type: "string",
+      default:{
+          value:null,
+          lookup:null
+      },
+      alias:'Age',
+      order:1,
+      prompt:'',
+      description:'',
+      errormessage:'',
+      isValid:{},
+      onChange:{}
+    },
+    socials: {
+      items: {
+        type: "string",
+        default:{
+            value:null,
+            lookup:null
+        },
+        alias:'Age',
+        order:1,
+        prompt:'',
+        description:'',
+        errormessage:'',
+        isValid:{},
+        onChange:{}
+      },
+      type: "array",
+      default:{
+          value:null,
+          lookup:null
+      },
+      alias:'Age',
+      order:1,
+      prompt:'',
+      description:'',
+      errormessage:'',
+      isValid:{},
+      onChange:{}
     }
-  }
+  },
+  required: [ "firstName", "lastName", "age", "socials"],
+  type: 'object',
 }
