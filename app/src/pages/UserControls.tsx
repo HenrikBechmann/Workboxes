0f13ca5e6c6712bf814826597a5ddf207690f4e6
Henrik Bechmann <henrik@therefore.ca>
2023-12-14 12:29:35 -0500

    add srawers to user controls

--- app/src/pages/UserControls.tsx
+++ app/src/pages/UserControls.tsx
@@ -1,14 +1,39 @@
 // UserControls.tsx
 // copyright (c) 2023-present Henrik Bechmann, Toronto, Licence: GPL-3.0
-import React from 'react'
+import React, { useState, useRef, useEffect, useCallback, CSSProperties } from 'react'
+import {Text, Button, Box} from '@chakra-ui/react'
 
-import { Box } from '@chakra-ui/react'
+// import Drawer from '../components/Drawer'
+import Drawer, { useDrawers } from '../components/Drawer'
 
-const outerStyle = {height: '100%', display:'relative'}
+const outerStyle = {height: '100%', position:'relative'} as CSSProperties
 
 const UserControls = (props) => {
 
-    return <Box data-type = 'sysadmin' style = {outerStyle}>User Controls</Box>
+   const {
+        drawerProps,
+        containerElementRef,
+        drawerState,
+        onOpens,
+    } = useDrawers()
+
+    // --------------------------- render --------------------
+
+    return <Box ref = {containerElementRef} data-type = 'usercontrols' style = {outerStyle}>
+        {drawerState != 'setup' && <>
+            <Drawer {...drawerProps.lookup} />
+            <Drawer {...drawerProps.data} />
+            <Drawer {...drawerProps.messages} />
+            <Drawer {...drawerProps.help} />
+        </>}
+        <Text>User Controls</Text>
+        <>
+        <Button onClick = {onOpens.openData} >Data</Button> 
+        <Button onClick = {onOpens.openLookup }>Lookup</Button> 
+        <Button onClick = {onOpens.openHelp}>Help</Button> 
+        <Button onClick = {onOpens.openMessages}>Messages</Button>
+        </>
+    </Box>
 
 }
 
