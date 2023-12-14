33d7ceb6dc94ae6eee300745b91b857dab2806f0
Henrik Bechmann <henrik@therefore.ca>
2023-12-14 15:26:29 -0500

    data boxes in usercontrols

--- app/src/pages/UserControls.tsx
+++ app/src/pages/UserControls.tsx
@@ -1,12 +1,27 @@
 // UserControls.tsx
 // copyright (c) 2023-present Henrik Bechmann, Toronto, Licence: GPL-3.0
 import React, { useState, useRef, useEffect, useCallback, CSSProperties } from 'react'
-import {Text, Button, Box} from '@chakra-ui/react'
+import {
+    Text, 
+    Button, 
+    Box, VStack,
+} from '@chakra-ui/react'
 
 // import Drawer from '../components/Drawer'
 import Drawer, { useDrawers } from '../components/Drawer'
 
-const outerStyle = {height: '100%', position:'relative'} as CSSProperties
+const outerStyle = {display: 'flex', flexWrap: 'wrap', height: '100%', position:'relative'} as CSSProperties
+
+const ContentBox = (props) => {
+
+    const {children} = props
+
+    return <Box style = {{flexBasis:'auto', flexShrink: 0, margin: '5px', backgroundColor:'white', height:'250px', width: '300px', border: '5px outset silver'}}>
+            <VStack>
+                {children}
+            </VStack>
+        </Box>
+}
 
 const UserControls = (props) => {
 
@@ -26,13 +41,24 @@ const UserControls = (props) => {
             <Drawer {...drawerProps.messages} />
             <Drawer {...drawerProps.help} />
         </>}
+        <ContentBox>
+        <VStack>
         <Text>User Controls</Text>
-        <>
         <Button onClick = {onOpens.openData} >Data</Button> 
         <Button onClick = {onOpens.openLookup }>Lookup</Button> 
         <Button onClick = {onOpens.openHelp}>Help</Button> 
         <Button onClick = {onOpens.openMessages}>Messages</Button>
-        </>
+        </VStack>
+        </ContentBox>
+        <ContentBox>
+            
+        </ContentBox>
+        <ContentBox>
+            
+        </ContentBox>
+        <ContentBox>
+            
+        </ContentBox>
     </Box>
 
 }
