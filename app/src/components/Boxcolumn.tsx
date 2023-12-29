880da74fa43cf501359eed759e3fed10280d9825
Henrik Bechmann <henrik@therefore.ca>
2023-12-29 07:57:43 -0500

    add boxclumn component

---
+++ app/src/components/Boxcolumn.tsx
@@ -0,0 +1,17 @@
+// Boxcolumn.tsx
+// copyright (c) 2023-present Henrik Bechmann, Toronto, Licence: GPL-3.0
+
+import React, { useState, useRef, useEffect, useCallback, CSSProperties } from 'react'
+
+import {
+    Text, 
+    Button, 
+    Input, FormControl, FormLabel, FormErrorMessage, FormHelperText,
+    Box, VStack, Center
+} from '@chakra-ui/react'
+
+const Boxcolumn = (props) => {
+    return <Box>Box column</Box>
+} 
+
+export default Boxcolumn
