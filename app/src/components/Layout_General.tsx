0fa29891a991fd464240099e2eeb6fabeb71d98d
Henrik Bechmann <henrik@therefore.ca>
2024-01-24 06:07:45 -0500

    file names

---
+++ app/src/components/Layout_General.tsx
@@ -0,0 +1,56 @@
+// GeneralLayout.tsx
+// copyright (c) 2023-present Henrik Bechmann, Toronto, Licence: GPL-3.0
+import React from 'react'
+import { Outlet, NavLink } from 'react-router-dom'
+import { Text, Box, Grid, GridItem } from '@chakra-ui/react'
+
+import { useUserData } from '../system/FirebaseProviders'
+
+import ToolbarFrame from '../components/toolbars/Toolbar_Frame'
+import ToolbarStandard from '../components/toolbars/Toolbar_Standard'
+
+const bodyStyle = {
+    height: '100%', 
+    display:'relative', 
+    backgroundColor:'aliceblue',
+    borderTop:'1px solid lightgray',
+    overflow:'hidden', // hide drawers
+}
+
+const navlinkStyles = { 
+    textDecoration:'underline', 
+    fontStyle:'italic', 
+    color:'blue',
+}
+
+const LayoutGeneral = (props) => {
+
+    const userData = useUserData()
+
+    if (userData === undefined) return null
+
+    return <Grid
+        data-type = 'layout-general'
+        height = '100vh' 
+        gridTemplateColumns = '1fr' 
+        gridTemplateRows = 'auto 1fr'
+        gridTemplateAreas = {`"header"
+                              "body"`}
+    >
+        <GridItem data-type = 'grid-header' gridArea = 'header'>
+            <ToolbarFrame>
+                {!userData && <Text ml = '6px'>Welcome to Tribalopolis! <NavLink to = '/signin'
+                style={navlinkStyles}
+                    >Sign in</NavLink></Text>}
+                {userData && <ToolbarStandard />}
+            </ToolbarFrame>
+        </GridItem>
+        <GridItem data-type = 'grid-body' gridArea = 'body'>        
+            <Box data-type = 'general-outlet' style = {bodyStyle}>
+                <Outlet />
+            </Box>
+        </GridItem>
+    </Grid>
+}
+
+export default LayoutGeneral
---
+++ app/src/components/Routes_Sysadmin.tsx
@@ -0,0 +1,37 @@
+// RoutesSysadmin.tsx
+// copyright (c) 2023-present Henrik Bechmann, Toronto, Licence: GPL-3.0
+import React from 'react'
+import { Navigate, Outlet, useLocation } from 'react-router-dom'
+import { Box } from '@chakra-ui/react'
+
+import { useUserData } from '../system/FirebaseProviders'
+
+function RoutesSysadmin() {
+
+  const 
+    userdata = useUserData(),
+    location = useLocation()
+
+  if (userdata === undefined) {
+
+      return <Box> Loading... </Box>
+      
+  }
+
+  if (!userdata) {
+
+    const from = location.pathname || '/'
+    return <Navigate to = {`/signin?from=${from}`}/>
+
+  } else if (userdata.sysadminStatus.isSuperUser) {
+
+    return <Outlet />
+
+  } else {
+
+    return <Navigate to = {`/unauthorized`} replace/>
+
+  }
+
+}
+export default RoutesSysadmin
