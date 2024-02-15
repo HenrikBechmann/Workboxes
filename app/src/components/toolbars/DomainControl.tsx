46bb1dd28c002ce152a141bc8ea8176eab493cf3
Henrik Bechmann <henrik@therefore.ca>
2024-02-15 11:33:37 -0500

    update

---
+++ app/src/components/toolbars/DomainControl.tsx
@@ -0,0 +1,34 @@
+// ItemControl.tsx
+// copyright (c) 2024-present Henrik Bechmann, Toronto, Licence: GPL-3.0
+
+import React, {CSSProperties} from 'react'
+
+import {
+    Tooltip, Box, Text,
+    Menu, MenuButton, MenuList, MenuItem, MenuDivider, MenuGroup
+} from '@chakra-ui/react'
+
+import domainIcon from '../../../assets/domain.png'
+
+const DomainControl = (props) => {
+
+    const { workboxDomainTitle } = props
+
+    const workboxItemIconStyles = {
+        width:'24px', 
+        height:'24px', 
+        borderRadius:'12px',
+    }
+
+   return <Box display = 'flex' flexDirection = 'column' justifyContent = 'center' alignContent = 'center'>
+        <Box display = 'flex' flexWrap = 'nowrap'>
+            <span>&nbsp;&nbsp;</span>
+            <img style = {workboxItemIconStyles} src = {domainIcon} />
+            <span>&nbsp;&nbsp;</span>
+            <Text fontSize = 'sm'>{workboxDomainTitle}</Text>
+        </Box>
+        <Box display = 'flex' justifyContent = 'center' fontSize = 'xs' color = 'gray' fontStyle = 'italic'><span>owner domain</span></Box>
+    </Box>
+}
+
+export default DomainControl
