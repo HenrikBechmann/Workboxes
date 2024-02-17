7eba7454f4dc253a81061a5fddc0e7929fb3b46f
Henrik Bechmann <henrik@therefore.ca>
2024-02-16 19:17:07 -0500

    update

--- app/src/system/system.type.tsx
+++ app/src/system/system.type.tsx
@@ -1,33 +1,35 @@
 export const metatype = {
-    class: "system",
-    variant: "type",
-    type: "system.type",
-    label: "System root type",
-    version: 0,
-    generation: 0,
-    relations: {
-        lists:[],
-        supers:[],
-        controllers:[],
-    },
-    read_group: "",
-    permissions: [],
-    counts: [],
-    commits: {
-        creator: "henrik",
-        created: {
-            nanoseconds: 630000000,
-            seconds: 1702416553,
+    systemdata: {
+        class: "system",
+        variant: "type",
+        type: "system.type",
+        label: "System root type",
+        version: 0,
+        generation: 0,
+        relations: {
+            lists:[],
+            supers:[],
+            controllers:[],
         },
-        updater: "henrik",
-        updated: {
-            nanoseconds: 859000000,
-            seconds: 1702416615,
+        read_group: "",
+        permissions: [],
+        counts: [],
+        commits: {
+            creator: "henrik",
+            created: {
+                nanoseconds: 630000000,
+                seconds: 1702416553,
+            },
+            updater: "henrik",
+            updated: {
+                nanoseconds: 859000000,
+                seconds: 1702416615,
+            },
         },
     },
     views:{},
     settings:{},
-    fields: {
+    data: {
         transitions:[],
         defaults:{
             system:{
@@ -60,136 +62,74 @@ export const metatype = {
                 settings:{},
             }
         },
-        metadata: {
-            views:{
-                xs:{
-                    component:"string",
-                    props:{},
-                    children:{},
+    },
+    metadata: {
+        views:{
+            xs:{
+                component:"string",
+                props:{},
+                children:{},
+            },
+            sm:{},
+            md:{},
+            lg:{},
+            xl:{},
+            edit:{},
+        },
+        settings:{
+        },
+        fields:{
+            name:"",
+            type: "map",
+            map_type: {
+                database: {
+                    name: "string",
+                    type: "string",
+                    value: "string | object",
+                    ingress:"namespace",
                 },
-                sm:{},
-                md:{},
-                lg:{},
-                xl:{},
-                edit:{},
-            },
-            settings:{
-            },
-            system:{
-                type: "map",
-                map_type: {
-                    name:"",
-                    database:{
-                        type: "string",
-                        value: "string",
-                        name:"string",
-                        ingress:"namespace"
-                    },
-                    model:{
-                        type: "string",
-                        value: "string",
-                        name:"string",
-                        ingress:"namespace"
-                    },
-                    display:{
-                        ingress:"namespace",
-                        views:{
-                            xs:{
-                                order:"number",
-                                component:"string",
-                                props:{},
-                                children:{},
-                                filter: "string",
-                            },
-                            sm:{},
-                            md:{},
-                            lg:{},
-                            xl:{},
-                        },
-                    },
-                    edit: {
-                        component:"RC",
-                        props:{},
-                        children:{},
-                        ingress:"namespace",
-                        error_message: "string",
-                        help_message:"string",
-                        min: "number",
-                        max: "number",
-                        minlength: "number",
-                        maxlength: "number",
-                        readonly: "boolean",
-                        required: "boolean",
-                        spellcheck: "",
-                        type: "string",
-                        default:{
-                            lookup: "namespace",
-                            value: "string",
-                        },
-                        change: "namespace",
-                        verify: "namespace",
-                        report: "namespace",
-                        filter: "string",
-                        placeholder: "",
-                        name: "string",
-                        order: "number",
-                        size: "number",
-                    },
+                model:{
+                    order:"number",
+                    required:"boolean",
+                    property:"string",
+                    type: "string",
+                    value: "string",
+                    label: "string",
+                    ingress: "namespace"
                 },
-            },
-            fields:{
-                name:"",
-                type: "map",
-                map_type: {
-                    database: {
-                        name: "string",
-                        type: "string",
-                        value: "string | object",
-                        ingress:"namespace",
-                    },
-                    model:{
-                        order:"number",
-                        required:"boolean",
-                        property:"string",
-                        type: "string",
+                edit: {
+                    error_message: "string",
+                    help_message:"string",
+                    min: "number",
+                    max: "number",
+                    minlength: "number",
+                    maxlength: "number",
+                    readonly: "boolean",
+                    required: "boolean",
+                    spellcheck: "",
+                    type: "string",
+                    default:{
+                        lookup: "namespace",
                         value: "string",
-                        label: "string",
-                        ingress: "namespace"
                     },
-                    edit: {
-                        error_message: "string",
-                        help_message:"string",
-                        min: "number",
-                        max: "number",
-                        minlength: "number",
-                        maxlength: "number",
-                        readonly: "boolean",
-                        required: "boolean",
-                        spellcheck: "",
-                        type: "string",
-                        default:{
-                            lookup: "namespace",
-                            value: "string",
-                        },
-                        ingress: "namespace",
-                        egress:"namespace",
-                        change: "namespace",
-                        verify: "namespace",
-                        filter: "string",
-                        format: "string",
-                        placeholder: "",
-                        name: "string",
-                        order: "number",
-                        size: "number",
-                    },
-                    display: {
-                        views:{
-                            xs:{},
-                            sm:{},
-                            md:{},
-                            lg:{},
-                            xl:{},
-                        },
+                    ingress: "namespace",
+                    egress:"namespace",
+                    change: "namespace",
+                    verify: "namespace",
+                    filter: "string",
+                    format: "string",
+                    placeholder: "",
+                    name: "string",
+                    order: "number",
+                    size: "number",
+                },
+                display: {
+                    views:{
+                        xs:{},
+                        sm:{},
+                        md:{},
+                        lg:{},
+                        xl:{},
                     },
                 },
             },
