42ee7981e4d2ec98885940403010064469c6a9ed
Henrik Bechmann <henrik@therefore.ca>
2024-02-23 13:48:47 -0500

    create Window component file

---
+++ app/src/components/Window.tsx
@@ -0,0 +1,14 @@
+// Window.tsx
+// copyright (c) 2024-present Henrik Bechmann, Toronto, Licence: GPL-3.0
+
+import React, { useState, useRef, useEffect, useCallback, CSSProperties } from 'react'
+
+import {
+    Box
+} from '@chakra-ui/react'
+
+const Window = (props) => {
+    return <Box>Workspace</Box>
+} 
+
+export default Window
