4d105ee948487be53f6d0fa9c76985d434c4a6b0
Henrik Bechmann <henrik@therefore.ca>
2024-03-07 20:48:05 -0500

    mainenance

--- app/src/components/workbox/WorkboxPanels.tsx
+++ app/src/components/workbox/WorkboxPanels.tsx
@@ -420,14 +420,6 @@ export const CoverPanel = forwardRef(function DocumentPanel(props:any, coverFram
             coverFrameElementRef.current.style.width = newWidth + 'px'
             coverWidthRef.current = newWidth
 
-            // const constraints = {
-            //     minX:MIN_PANEL_FRAME_WIDTH,
-            //     minY:coverFrameElementRef.current?.offsetHeight || 0,
-            //     maxX:centralPanelElementRef.current.offsetWidth * MAX_PANEL_FRAME_RATIO,
-            //     maxY:coverFrameElementRef.current?.offsetHeight || 0,
-            // }
-            // constraintsRef.current = constraints
-
             setCoverResizeWidth(newWidth)
 
         }
