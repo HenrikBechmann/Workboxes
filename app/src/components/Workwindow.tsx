ba62824a0b2835e6720c991982035367045909c0
Henrik Bechmann <henrik@therefore.ca>
2024-03-12 16:07:10 -0400

    code maintenance

--- app/src/components/Workwindow.tsx
+++ app/src/components/Workwindow.tsx
@@ -1,7 +1,7 @@
 // workwindow.tsx
 // copyright (c) 2024-present Henrik Bechmann, Toronto, Licence: GPL-3.0
 
-import React, { useState, useRef, useEffect, useCallback, createContext, CSSProperties } from 'react'
+import React, { useState, useRef, useEffect, useCallback, CSSProperties } from 'react'
 
 import {
     Grid, GridItem,
--- app/src/components/workbox/WorkboxPanels.tsx
+++ app/src/components/workbox/WorkboxPanels.tsx
@@ -6,7 +6,6 @@ import React, {
     useEffect, 
     useState, 
     useCallback, 
-    createContext, 
     useContext, 
     CSSProperties, 
     forwardRef 
