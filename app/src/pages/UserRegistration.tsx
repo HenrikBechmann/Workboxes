ab417431f25551d3eccfee456ec1545f90796e9a
Henrik Bechmann <henrik@therefore.ca>
2024-04-19 04:25:29 -0400

    registration forms

--- app/src/pages/UserRegistration.tsx
+++ app/src/pages/UserRegistration.tsx
@@ -10,11 +10,52 @@ import { signOut } from "firebase/auth"
 import { 
     Box, Text, Heading,
     Tabs, TabList, Tab, TabPanels, TabPanel,
-    Button, Link,
+    Button, Link, Checkbox,
+    useDisclosure,
+    AlertDialog, AlertDialogOverlay, AlertDialogContent, AlertDialogHeader, AlertDialogBody, AlertDialogFooter
 } from '@chakra-ui/react'
 
 import { useUserData, useUserRecords, useAuth } from '../system/FirebaseProviders'
 
+const HandleAlert = (props) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const cancelRef = React.useRef()
+
+  return (
+    <>
+      <Button colorScheme='blue' onClick={onOpen}>
+        Save User Handle
+      </Button>
+
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+              Save User Handle
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Are you sure? You can't undo this action afterwards.
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme='blue' onClick={onClose} ml={3}>
+                Save
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
+  )
+}
 const UserRegistration = (props) => {
 
     const 
@@ -43,6 +84,7 @@ const UserRegistration = (props) => {
 
     }
 
+
     return <>
         <Box padding = '6px'>
         <Heading mb = '3px' size = 'md'>Welcome to the Workboxes app!</Heading>
@@ -53,10 +95,10 @@ const UserRegistration = (props) => {
         </Text>
 
         <Text mt = '6px'>
-            Thank you for joining the Workboxes app! There are a couple of things we need before you get going.
+            Thank you for joining the Workboxes app! There are a few things we need from you before you get going.
         </Text>
         <Text mt = '6px'>
-            1. A user "handle" will provide other users with a way to reach you, and refer to you.
+            1. A permanent user "handle" will provide other users with a way to reach you, and refer to you.
             You'll have lots of ways to control this.
         </Text>
         <Text mt = '6px'>
@@ -64,10 +106,12 @@ const UserRegistration = (props) => {
             [Again, this is pending -- the app is free for now for invited guests.]
         </Text>
         <Text>3. Accept our (somewhat preliminary) terms and conditions.</Text>
-        <Text mt = '6px'>
+        <Text mt = '6px' mb = '6px'>
             You'll be able to continue to the app when the user handle and terms have been accepted. 
             [In future the payment method will need to be accepted as well].
         </Text>
+        <em>Accepted: <Checkbox isChecked = {true}>User Handle</Checkbox> 
+        <Checkbox isChecked = {false} ml = '10px'>Terms and Conditions</Checkbox></em>
         </Box >
         <Tabs variant = 'enclosed' margin = '3px'>
             <TabList>
@@ -80,7 +124,8 @@ const UserRegistration = (props) => {
                     <Text>
                         Handle: required: handle (immutable), name; 
                         optional: location, description, keyword, birth day; 
-                        automated: date joined                        
+                        automated: date joined  
+                        <HandleAlert />                      
                     </Text>      
                 </TabPanel>
                 <TabPanel>
@@ -92,11 +137,19 @@ const UserRegistration = (props) => {
                 </TabPanel>
                 <TabPanel>
                     <Text>
-                        [pending] Terms and Conditions. Basically, be constructive, be polite, be honest. Avoid misinformation. 
+                        [preliminary] Terms and Conditions. Be constructive, be polite, be honest. Avoid misinformation. 
                         Disinformation (deliberately misleading information) won't be tolerated. Phishing or any devious 
-                        practices won't be tolerated. We reserve the right to cancel your participation here at any time 
-                        for any reason. My house, my rules. If we ever get big enough, we'll have an ethics committee.
+                        practices won't be tolerated. No spam please. 
                     </Text>
+                    <Text mt = '6px'>
+                        We reserve the right to cancel your participation here at any time for any reason. 
+                        If we ever get big enough, we'll have an ethics committee to oversee this.
+                    </Text>
+                    <Text mt = '6px' mb = '6px'>
+                        I think that assets uploaded here will have to stay here under some kind of Creative Commons licence.
+                        In the meantime refrain from uploading material which you consider to be private.
+                    </Text>
+                    <Text><Button colorScheme = 'blue'>I accept</Button> these terms and conditions.</Text>
                 </TabPanel>
             </TabPanels>
         </Tabs>
