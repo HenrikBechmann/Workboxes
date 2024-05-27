661e2118acb6c478573dc08aa1b5da342587fd37
Henrik Bechmann <henrik@therefore.ca>
2024-05-27 06:05:05 -0400

    write member and member workbox records

--- app/src/pages/Administration.tsx
+++ app/src/pages/Administration.tsx
@@ -139,8 +139,7 @@ const Administration = (props) => {
                       id:userRecord.profile.user.id,
                       name: userRecord.profile.user.name,
                   },
-                  updated_timestamp: serverTimestamp(),
-                  
+                  updated_timestamp: serverTimestamp(),                  
                 },
                 counts:{
                     workboxes: 1
@@ -149,7 +148,89 @@ const Administration = (props) => {
             }}
         )
 
-        console.log('updatedMemberRecord',memberRecord)
+        const memberWorkbox = updateDocumentSchema('members','standard',{},{
+
+            profile: {
+                workbox: {
+                  id: memberWorkboxRef.id,
+                  name: userRecord.profile.user.name,
+                  image: {
+                    source: userRecord.profile.user.image.source
+                  },
+                },
+                user: {
+                  id:userRecord.profile.user.id,
+                  name: userRecord.profile.user.name,
+                },
+                member: {
+                  id:memberRecordRef.id,
+                  name: userRecord.profile.user.name,
+                },
+                owner: {
+                  id:userRecord.profile.user.id,
+                  name: userRecord.profile.user.name,
+                },
+                domain: {
+                  id: userRecord.profile.domain.id,
+                  name: userRecord.profile.domain.name,
+                },
+                type: {
+                  name: "member",
+                  alias: "Member",
+                  image: {
+                    source: null,
+                  },
+                },
+                commits: {
+                  created_by: {
+                      id:userRecord.profile.user.id,
+                      name: userRecord.profile.user.name,
+                  },
+                  created_timestamp: serverTimestamp(),
+                  updated_by: {
+                      id:userRecord.profile.user.id,
+                      name: userRecord.profile.user.name,
+                  },
+                  updated_timestamp: serverTimestamp(),
+                },
+            },
+            document: {
+                sections: [
+                  {
+                    name: "standard",
+                    alias: "Standard",
+                    position: 0,
+                    data: {
+                      name: userRecord.profile.user.name,
+                      image: {
+                        source: userRecord.profile.user.image.source
+                      },
+                      description: userRecord.profile.user.description,
+                      summary: null,
+                    },
+                  },
+                ],
+            },
+        })
+
+        console.log('memberRecord, memberWorkbox',memberRecord, memberWorkbox)
+
+        // ------------------[ database interaction ]-----------------
+        const batch = writeBatch(db)
+        try {
+            batch.set(memberRecordRef,memberRecord)
+            batch.set(memberWorkboxRef,memberWorkbox)
+            await batch.commit()
+        } catch (error) {
+            const errdesc = 'administration error writing memberRecord and memberWorkbox to database'
+            console.log(errdesc, error)
+            errorControl.push({description:errdesc, error})
+            navigate('/error')
+            return
+        }
+        // --------------------
+
+        alert('Done.')
     }
 
     return <Box data-type = 'page-content' width = '100%' display = 'flex' flexWrap = 'wrap'>
