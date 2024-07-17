1a0bcfecd6754f2ad6bd31bdd0e0055a4825d533
Henrik Bechmann <henrik@therefore.ca>
2024-07-17 10:59:41 -0400

    maintenance

--- app/src/components/documentUI/DocumentBase.tsx
+++ app/src/components/documentUI/DocumentBase.tsx
@@ -15,9 +15,9 @@ import { useCreateBlockNote } from "@blocknote/react";
 import { BlockNoteView } from "@blocknote/mantine";
 import "@blocknote/mantine/style.css";
 
-// import {useDropzone} from 'react-dropzone'
-// import ReactCrop from 'react-image-crop'
-// import 'react-image-crop/dist/ReactCrop.css'
+import {useDropzone} from 'react-dropzone'
+import ReactCrop from 'react-image-crop'
+import 'react-image-crop/dist/ReactCrop.css'
 
 import { useSystemRecords } from '../../system/WorkboxesProvider'
 import { useWorkboxHandler } from '../workbox/Workbox'
