a1eb10e942e402d25246c94f26e0310e01eb7ae6
Henrik Bechmann <henrik@therefore.ca>
2024-09-17 14:21:13 -0400

    scaffold base data display and edit

---
+++ app/src/components/documentUI/BaseDataDisplay.tsx
@@ -0,0 +1,10 @@
+// BaseDataDisplay.tsx
+// copyright (c) 2024-present Henrik Bechmann, Toronto, Licence: GPL-3.0
+
+import React from 'react'
+
+const BaseDataDisplay = () => {
+    return <div>base data display</div>
+}
+
+export default BaseDataDisplay
---
+++ app/src/components/documentUI/BaseDataEdit.tsx
@@ -0,0 +1,10 @@
+// BaseDataEdit.tsx
+// copyright (c) 2024-present Henrik Bechmann, Toronto, Licence: GPL-3.0
+
+import React from 'react'
+
+const BaseDataEdit = () => {
+    return <div>base data edit</div>
+}
+
+export default BaseDataEdit
