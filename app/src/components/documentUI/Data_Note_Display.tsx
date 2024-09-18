3034489aac074894f649a94029fd95f515d4f2ee
Henrik Bechmann <henrik@therefore.ca>
2024-09-18 13:24:37 -0400

    display empty blocknote

--- app/src/components/documentUI/Data_Note_Display.tsx
+++ app/src/components/documentUI/Data_Note_Display.tsx
@@ -1,7 +1,7 @@
 // Data_Note_Display.tsx
 // copyright (c) 2024-present Henrik Bechmann, Toronto, Licence: GPL-3.0
 
-import React, {useRef, useState, useEffect, CSSProperties} from 'react'
+import React, {useRef, useEffect, useState} from 'react'
 
 import "@blocknote/core/fonts/inter.css";
 import { 
@@ -18,17 +18,24 @@ const DataNoteDisplay = () => {
         [workboxHandler, dispatchWorkboxHandler] = useWorkboxHandler(),
         { workboxRecord } = workboxHandler,
         documentData = workboxRecord.document.data,
-        editor = useCreateBlockNote({initialContent:documentData.content || [{}], trailingBlock:false})
+        editor = useCreateBlockNote({initialContent:documentData.content || [{}], trailingBlock:false}),
+        [isEmpty, setIsEmpty] = useState(false)
 
     useEffect(()=>{
 
-        const data = documentData.content || [{}]
+        const data = documentData.content || [{}],
+            block = data[0]
 
         editor.replaceBlocks(editor.document,data)
 
+        const isEmpty = (!documentData.content || 
+            (data.length == 1 && block.type == 'paragraph' && block.content.length == 0))
+
+        setIsEmpty( isEmpty )
+
     },[documentData.content])
 
-    return <div><BlockNoteView editor = { editor } editable = { false } sideMenu = { false }/></div>
+    return <div>{(!isEmpty) && <BlockNoteView editor = { editor } editable = { false } sideMenu = { false }/>}</div>
 
 }
 
