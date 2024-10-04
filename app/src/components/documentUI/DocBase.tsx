481760e89da141a8793894840bbc01953212403e
Henrik Bechmann <henrik@therefore.ca>
2024-10-04 08:36:06 -0400

    start controller animation

--- app/src/components/documentUI/DocBase.tsx
+++ app/src/components/documentUI/DocBase.tsx
@@ -86,6 +86,45 @@ const alternateActionIconStyles = {
     top:'36px',
 } as CSSProperties
 
+const animateTransition = 'height .5s'
+
+const animateModeChange = (element) => {
+
+    const sectionHeight = element.firstChild.scrollHeight;
+  
+    element.style.transition = animateTransition
+
+    requestAnimationFrame(() => {
+        element.style.height = sectionHeight + 'px';
+    });
+
+    setTimeout(()=>{
+        element.style.transtion = ''
+        element.style.height = 'auto'
+    },600)
+  
+}
+
+// function expandSection(element) {
+//   // get the height of the element's inner content, regardless of its actual size
+//   var sectionHeight = element.scrollHeight;
+  
+//   // have the element transition to the height of its inner content
+//   element.style.height = sectionHeight + 'px';
+
+//   // when the next css transition finishes (which should be the one we just triggered)
+//   element.addEventListener('transitionend', function(e) {
+//     // remove this event listener so it only gets triggered once
+//     element.removeEventListener('transitionend', arguments.callee);
+    
+//     // remove "height" from the element's inline styles, so it can return to its initial value
+//     element.style.height = null;
+//   });
+  
+//   // mark the section as "currently not collapsed"
+//   element.setAttribute('data-collapsed', 'false');
+// }
+
 // edit
 
 const Base_Edit_Todo = (props) => {
@@ -509,14 +548,33 @@ const TodoController = (props) => {
     const 
         { controlPack, todo } = props,
         { mode } = controlPack,
-        animationBoxRef = useRef(null)
+        animationBoxRef = useRef(null),
+        isInitializedRef = useRef(false),
+        [newMode, setNewMode] = useState('mode')
 
     useEffect(()=>{
 
+        if (!isInitializedRef.current) {
+            isInitializedRef.current = true
+            return
+        }
+
+        const startingHeight = animationBoxRef.current.scrollHeight
+        animationBoxRef.current.style.height = startingHeight + 'px'
+
+        setNewMode(mode)
+
     },[mode])
 
+
+    useEffect(()=>{
+
+        animateModeChange(animationBoxRef.current)
+
+    },[newMode])
+
     return <Box ref = {animationBoxRef} ><Box>
-        {(mode !='edit') 
+        {(newMode !='edit') 
             ? <Base_Display_Todo todo = {todo}/>
             : (controlPack.currentEditBlockID === controlPack.blockIDMap.get('todo'))
                 ? <Base_Edit_Todo todo = {todo} controlPack = { controlPack }/>
